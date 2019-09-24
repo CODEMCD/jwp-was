04e8785abb5e385a39fccbc767cdea0362b3776a
CODEMCD <psbum1609@gmail.com>
2019-09-24 23:05:21 +0900

    refactor: HTTP Version 객체 분리

---
+++ src/main/java/http/HttpVersion.java
@@ -0,0 +1,27 @@
+package http;
+
+import http.exception.NotFoundVersionException;
+
+import java.util.Arrays;
+
+public enum HttpVersion {
+    V_1_1(1.1, "HTTP/1.1");
+
+    private Double versionNumber;
+    private String version;
+
+    HttpVersion(Double versionNumber, String version) {
+        this.versionNumber = versionNumber;
+        this.version = version;
+    }
+
+    public static HttpVersion of(String version) {
+        String[] versionTokens = version.split("\\/");
+        Double versionTokenNumber = Double.parseDouble(versionTokens[1]);
+        return Arrays.stream(HttpVersion.values())
+                .filter(httpVersion -> httpVersion.versionNumber.equals(versionTokenNumber))
+                .findAny()
+                .orElseThrow(NotFoundVersionException::new)
+                ;
+    }
+}
