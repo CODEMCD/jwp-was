c4ee7a10660dba2f2ee5bdb28a4728aa43e7c652
CODEMCD <psbum1609@gmail.com>
2019-09-25 13:29:36 +0900

    refactor: HTTP body 분리

---
+++ src/main/java/http/HttpBody.java
@@ -0,0 +1,21 @@
+package http;
+
+public class HttpBody {
+    private static final String QUERY_STRING_CONTENT_TYPE = "application/x-www-form-urlencoded";
+    private static final String CONTENT_TYPE_KEY = "Content-Type";
+
+    private String body;
+
+    public HttpBody(String body) {
+        this.body = body;
+    }
+
+    public boolean addQueryString(HttpHeader httpHeader, QueryString queryString) {
+        if (!QUERY_STRING_CONTENT_TYPE.equals(httpHeader.getValue(CONTENT_TYPE_KEY))) {
+            return false;
+        }
+
+        queryString.add(body);
+        return true;
+    }
+}
